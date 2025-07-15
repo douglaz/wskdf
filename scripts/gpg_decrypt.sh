7b401667ac4f08fc33641abfb83641af88c88256
Allan Douglas <allandouglas@gmail.com>
2025-07-15 20:29:41 +0000

    chore: minor improvement in script

--- scripts/gpg_decrypt.sh
+++ scripts/gpg_decrypt.sh
@@ -2,6 +2,10 @@
 # This will come from the environment variables
 input_file=$INPUT_FILE
 output_file=$OUTPUT_FILE
+if [[ ! -f "$input_file" ]]; then
+    echo "Input file $input_file does not exist"
+    exit 1
+fi
 # Note: we could call gpg directly with the key on stdin, but we want to log the key for debugging purposes
 read -r key
 echo "Trying to decrypt $input_file with key $key"
